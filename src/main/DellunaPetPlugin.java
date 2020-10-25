b3d7370e92efe8d681f2db8cfcbe883e36cfb834
Simyeon <atoz0823@gmail.com>
2020-10-25 22:09:41 +0900

    Rename Plugin Base File

--- src/main/DellunaPetPlugin.java
+++ src/main/DellunaPetPlugin.java
@@ -2,7 +2,7 @@ package main;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class DellunaPet extends JavaPlugin {
+public class DellunaPetPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         System.out.println("[DP] Plugin Test");
